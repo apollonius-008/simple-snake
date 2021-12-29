3faacbc26eaf2c20a78a14c00bf243065597a102
Sarit Kumar Das <saritkdas1729@gmail.com>
2021-12-29 19:48:16 +0530

    ADD BOARD CLASS FOR MAINTAINING BOARD INFO

--- simple_snake/logic.py
+++ simple_snake/logic.py
@@ -1,4 +1,5 @@
 import random
+from typing import Deque
 
 def pos_after_move(pos: dict, move: str):
     if move == 'up':
@@ -35,6 +36,15 @@ def get_opposite_move(move):
     else:
         print(f"get_opposite_move(move) :: Invalid Move. move={move}")
 
+def create_empty_board(board_width, board_height):
+    board = []
+    for i in range(board_height):
+        dummy = []
+        for j in range(board_width):
+            dummy.append(0)
+        board.append(dummy)
+    return board
+
 def avoid_snakes_neck(last_move, possible_move):
     if last_move == None:
         return possible_move
@@ -43,16 +53,22 @@ def avoid_snakes_neck(last_move, possible_move):
         possible_move.remove(op_move)
         return possible_move
 
+def set_board_state(board, pos, state):
+    board[pos['y']][pos['x']] = state
+
+def get_board_state(board, pos):
+    return board[pos['y']][pos['x']]
+
 class Player:
 
     def __init__(self, data):
         self.my_snake = data['you']
-        self.board = data['board']
+        self.board = Board(data['board'])
         self.last_move = None
 
     def update(self, data):
         self.my_snake = data['you']
-        self.board = data['board']
+        self.board.update(data['board'])
     
     def get_move(self):
         possible_moves = ['up', 'down', 'left', 'right']
@@ -70,8 +86,84 @@ class Player:
         return possible_moves
 
     def avoid_walls(self, possible_moves):
-        return avoid_walls(self.my_snake['head'], self.board['width'], self.board['height'], possible_moves)
+        return avoid_walls(self.my_snake['head'], self.board.width, self.board.height, possible_moves)
     
     def avoid_neck(self, possible_moves):
         return avoid_snakes_neck(self.last_move, possible_moves)
+
+class Board:
+
+    def __init__(self, board):
+        self.width = board['width']
+        self.height = board['height']
+        
+        self.foods = board['food']
+        self.create_food_board(board)
+
+        self.snakes = {}
+        for snake in board['snakes']:
+            self.snakes[snake['id']] = snake
+        self.create_snake_board(board)
+
+    def create_food_board(self, board):
+        self.food_board = create_empty_board(self.width, self.height)
+        for food in self.foods:
+            set_board_state(self.food_board, food, 1)
+
+    def create_snake_board(self, board):
+        self.snake_board = create_empty_board(self.width, self.height)
+        for snake in board['snake']:
+            for part in snake['body']:
+                set_board_state(self.snake_board, part, snake['id'])
+
+    def is_pos_snake(self, pos):
+        return type(get_board_state(self.snake_board, pos)) == str
+
+    def is_pos_food(self, pos):
+        return get_board_state(self.food_board, pos) == 1
+
+    def update(self, board):
+        self.update_snakes(board)
+        self.update_food(board)
+
+    def update_food(self, board):
+        for food in self.foods:
+            set_board_state(self.food_board, food, 0)
+        self.foods = board['food']
+        for food in self.foods:
+            set_board_state(self.food_board, food, 1)
     
+    def update_food_eating_status(self, board):
+        for snake_id in self.snakes:
+            head = self.snakes[snake_id]['head']
+            if self.is_pos_food(head):
+                self.snakes[snake_id]['ate'] = True
+            else:
+                self.snakes[snake_id]['ate'] = False
+
+    def update_snakes(self, board):
+        deleted_snakes = self.deleted_snakes(board)
+        for snake_id in deleted_snakes:
+            for part in self.snakes[snake_id]['body']:
+                set_board_state(self.snake_board, part, 0)
+        
+        for snake in board['snakes']:
+            if self.snakes[snake['id']]['body'][-1] != snake['body'][-1]:
+                set_board_state(self.snake_board, self.snakes[snake['id']]['body'][-1], 0)
+            set_board_state(self.snake_board, snake['head'], snake['id'])
+
+        self.snakes = {}
+        for snake in board['snakes']:
+            self.snakes[snake['id']] = snake
+
+        self.update_food_eating_status(board)
+
+    def deleted_snakes(self, board):
+        deleted_snakes = []
+        for snake_id in self.snakes:
+            for snake in board['snakes']:
+                if snake['id'] == snake_id:
+                    break
+            else:
+                deleted_snakes.append(snake_id)
+        return deleted_snakes
