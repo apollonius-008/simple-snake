fd868404ca927c3e2969db18a29f09446fb4c2bd
Sarit Kumar Das <saritkdas1729@gmail.com>
2021-12-31 12:09:54 +0530

    ADD PARTIAL CODE FOR TESTING WITH GAME DATA

--- tests/test_simple_snake.py
+++ tests/test_simple_snake.py
@@ -1,5 +1,25 @@
 from simple_snake import __version__
+from ..simple_snake.logic import Player
+import os
+import json
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def get_all_test_files():
+    path = "tests"
+    all_test_files = []
+    for x in os.listdir(path):
+        if x.endswith('.json'):
+            all_test_files.append(x)
+    return all_test_files
+
+def test_from_files():
+    all_test_files = get_all_test_files()
+    for fname in all_test_files:
+        with open(f"tests\\{fname}", 'r') as file:
+            games = json.load(file)
+            for data in games:
+                pass
+
